f139a78baa20c55e0403924ad9a214f5a5086632
OtaniRed <39827443+OtaniRed@users.noreply.github.com>
2018-09-06 11:00:02 +0300

    добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/testerjava/sandbox/Myfirstprogram.java
+++ sandbox/src/main/java/ru/stqa/testerjava/sandbox/Myfirstprogram.java
@@ -4,9 +4,33 @@ public class Myfirstprogram
 {
    public static void main(String[] args)
 {
+ hello("world");
+ hello("user");
+ hello("Vadim");
 
-  System.out.println("Hello World");
+ double l = 5;
+  System.out.println("Площадб квадрата со стороно " + l + " = " + area(l));
 
-  
+ double a = 4;
+ double b = 6;
+  System.out.println("Площадб прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
 }
+
+public static void hello(String somebody)
+
+{
+  System.out.println("Hello, " + somebody + "!");
+}
+
+public static double area(double len)
+{
+  return len * len;
+}
+
+public static double area(double a, double b)
+{
+  return a * b;
+}
+
 }
