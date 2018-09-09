af4baebb5345fd99d482e9ac027956dea721cb2f
OtaniRed <39827443+OtaniRed@users.noreply.github.com>
2018-09-09 18:03:05 +0300

    первый урок пройден, плюс написан 1 тест

--- sandbox/src/main/java/ru/stqa/testerjava/sandbox/Myfirstprogram.java
+++ sandbox/src/main/java/ru/stqa/testerjava/sandbox/Myfirstprogram.java
@@ -9,10 +9,10 @@ public class Myfirstprogram
  hello("Vadim");
 
  Square s = new Square(5);
-  System.out.println("Площадб квадрата со стороно " + s.l + " = " + s.area());
+  System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
   Rectangle r = new Rectangle(4, 6);
-  System.out.println("Площадб прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
 }
 
